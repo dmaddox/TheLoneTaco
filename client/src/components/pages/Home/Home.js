017047936d8edd06ef9a72006483084c08c57c05
dmaddox <dmaddox@gmail.com>
2018-05-14 21:16:01 -0500

    basic file structure

---
+++ client/src/components/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component} from "react";
+// import {Jumbotron} from "../../layout/Jumbotron";
+
+class Home extends Component {
+  render() {
+    return (
+      
+        <div>
+        Welcome home.
+        </div>
+      
+      
+    )
+  }
+}
+
+export default Home;
