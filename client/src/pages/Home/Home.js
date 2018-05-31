0417257ab7143ef1c038f246bdcdec015077b6b7
dmaddox <dmaddox@gmail.com>
2018-05-31 17:25:46 -0500

    navbar class component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,13 @@
 import React, { Component} from "react";
 import Jumbotron from "../../components/layout/Jumbotron";
+import Navbar from "../../components/layout/Navbar";
 import InitialSearch from "../../components/forms/InitialSearch";
 
 class Home extends Component {
   render() {
     return (
+      <div>
+      <Navbar />
       <Jumbotron>
         <div>
         Welcome home.
@@ -12,7 +15,7 @@ class Home extends Component {
         <InitialSearch>
         </InitialSearch>
       </Jumbotron>
-      
+      </div>
     )
   }
 }
